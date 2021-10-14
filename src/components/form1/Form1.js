220582f0e0938bc1642e899e47ccf7709783e9e1
crbwebdevonly <crbwebdevonly@gmail.com>
2021-10-14 16:15:29 +0100

    added validation

--- src/components/form1/Form1.js
+++ src/components/form1/Form1.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form1.scss";
 //======================================================
 
@@ -7,16 +7,51 @@ import "./form1.scss";
 
 const Form1 = () => {
      //======================================================
+     const initialSubmitData = {
+          username: "",
+          email: "",
+          password: "",
+     };
+     const [submitData, setSubmitData] = useState(initialSubmitData);
+     //======================================================
+     //======================================================
+     const handleChange = (e) => {
+          // console.log(e.target.name,e.target.value);
+          setSubmitData({ ...submitData, [e.target.name]: e.target.value });
+     };
+     //======================================================
+     //======================================================
+     const [errors,setErrors] = useState({})
+     //====================================================== 
+     const formValidate = (inObj)=>{
+          const errors = {}
+          console.log("inObj.username>>",inObj.username);
+          if(!inObj.username){
+               errors.username = "must provide username"
+          }
+          if(!inObj.email){
+               errors.email = "must provide email"
+          }
+          if(!inObj.password){
+               errors.password = "must provide password"
+          }
+// console.log("toset errors:::>",errors);
+          return errors
+     }
+     //======================================================
+     //======================================================
+     const handleSubmit = (e) => {
+          e.preventDefault();
+          // errors
+          setErrors(formValidate(submitData))
+        
+     };
      //======================================================
      //======================================================
      //======================================================
-     //======================================================
-     //======================================================
-     //======================================================
-     //======================================================
-     //======================================================
-     //======================================================
-     //======================================================
+     console.log(submitData);
+     console.log(errors);
+     console.log("Object.keys(errors)",  Object.keys(errors))
      //======================================================
      //======================================================
      return (
@@ -24,25 +59,49 @@ const Form1 = () => {
                <div className="form-wrap">
                     <h1 className="title">Form1</h1>
                     <hr />
-                    <form>
+                    <form onSubmit={handleSubmit}>
                          <ul>
                               <li>
                                    <label htmlFor="">Name</label>
-                                   <input type="text" />
-                                   <div className="error-message">error messgae</div>
+                                   <input
+                                        type="text"
+                                        name="username"
+                                        onChange={handleChange}
+                                        value={submitData.name}
+                                   />
+                                   <div className="error-message">
+                                   {errors.username}
+                                   </div>
                               </li>
                               <li>
                                    <label htmlFor="">Email</label>
-                                   <input type="text" />
-                                   <div className="error-message">error messgae</div>
+                                   <input
+                                        type="text"
+                                        name="email"
+                                        onChange={handleChange}
+                                        value={submitData.email}
+                                   />
+                                   <div className="error-message">
+                                   {errors.email}
+                                   </div>
                               </li>
                               <li>
-                                   <label htmlFor="">Passowrd</label>
-                                   <input type="text" />
-                                   <div className="error-message">error messgae</div>
+                                   <label htmlFor="">Password</label>
+                                   <input
+                                        type="text"
+                                        name="password"
+                                        onChange={handleChange}
+                                        value={submitData.password}
+                                   />
+                                   <div className="error-message">
+                                   {errors.password}
+                                   </div>
+                                   <hr />
                               </li>
                          </ul>
-                        <button className="button1">Submit</button>
+
+                         <button className="button1">Submit</button>
+                         {Object.keys(errors).length === 0 && <div className="success">Submit success</div>}
                     </form>
                </div>
           </div>
